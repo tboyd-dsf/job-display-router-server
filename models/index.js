958a77acb683cbcd146d3a6a681d084eb530bd49
Tyler Boyd <tylerb@theboydprojects.com>
2020-06-04 08:05:00 -0500

    refigured sequelize variable to represent env

--- models/index.js
+++ models/index.js
@@ -8,15 +8,15 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 let db = {};
 
-let sequelize = new Sequelize(
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     config
-);
-
-if (config.use_env_variable) {
-     sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  );
 }
 
 fs.readdirSync(__dirname)
